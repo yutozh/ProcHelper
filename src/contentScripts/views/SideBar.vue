2974694589320a61cab9f7fdca2ae2020145de9d
yuto_zh <545023318@qq.com>
2023-06-26 22:50:27 +0800

    SideBar update

--- src/contentScripts/views/SideBar.vue
+++ src/contentScripts/views/SideBar.vue
@@ -2,25 +2,43 @@
 <script setup lang="ts">
 import 'uno.css'
 import { ref } from 'vue'
-import { ElDrawer } from 'element-plus'
+import { ElDrawer, ElMessage } from 'element-plus'
 import { verifyFun } from '~/logic/verification'
+import { storageConf } from '~/logic/storage'
 
 const activeCollapse = ref([''])
 const table = ref(false)
+const loading = ref(false)
+const verifiedCount = ref(0)
 const gridData: { [key: string]: string }[] = reactive([])
-const verification = [
-  {
-    id: 'a',
-    name: '测试1',
-    locateMethod: 'querySelector',
-    location: '#entry-nqv-eoe-vhg',
-    verifyMethod: 'hasDigit',
-  },
-]
+const verification = storageConf.value.verification
+
+function onControlBtnClick() {
+  if (table.value) {
+    table.value = false
+    return
+  }
+  // 只有当前页面是审批详情页时，才打开侧边栏
+  const conConfig = storageConf.value.form_container
+  if (conConfig.locate_method === 'querySeletor') {
+    const container = document.querySelector(conConfig.location)
+    if (container && getComputedStyle(container).display !== 'none') {
+      table.value = true
+      return
+    }
+  }
+  ElMessage({
+    message: '请处于审批单详情页时再点击此按钮.',
+    type: 'warning',
+  })
+}
 
 function onOpenFun() {
-  console.log('okokok')
+  loading.value = true
+  verifiedCount.value = 0
+  gridData.splice(0, gridData.length)
   for (const item of verification) {
+    verifiedCount.value += 1
     if (item.locateMethod === 'querySelector') {
       const ele = document.querySelector(item.location)
       console.log(ele)
@@ -36,7 +54,9 @@ function onOpenFun() {
         console.log(result)
         console.log(detail)
         gridData.push({
-          name: item.name,
+          verifyObj: item.verifyObj,
+          verifyInfo: item.verifyInfo,
+          result,
           detail,
         })
         console.log(gridData)
@@ -46,6 +66,9 @@ function onOpenFun() {
       }
     }
   }
+  setTimeout(() => {
+    loading.value = false
+  }, 1000)
 }
 
 function handleResBtnLocate() {
@@ -56,13 +79,13 @@ function handleResBtnLocate() {
 <template>
   <div class="fixed right-0 bottom-0 m-5 z-100 flex items-end font-sans select-none leading-1em">
     <el-button
-      class="flex w-10 h-10 rounded-full shadow cursor-pointer border-none"
-      bg="teal-600 hover:teal-700"
-      @click="table = true"
+      class="flex w-10 h-10 rounded-full shadow cursor-pointer border-none teal-600 hover:teal-700"
+      @click="onControlBtnClick"
     >
       <pixelarticons-power class="block m-auto text-white text-lg" />
     </el-button>
   </div>
+
   <ElDrawer
     v-model="table"
     title="流程助手"
@@ -74,69 +97,78 @@ function handleResBtnLocate() {
     modal-class="drawer-modal"
     @open="onOpenFun"
   >
-    <el-table :data="gridData" table-layout="fixed">
-      <el-table-column property="name" label="检查项" width="150" />
-      <el-table-column property="detail" label="结果" />
-      <el-table-column property="detail" label="操作" fixed="right">
-        <template #default>
-          <el-tooltip
-            class="box-item"
-            effect="light"
-            content="定位"
-            placement="top"
-          >
-            <el-button link type="primary" size="small" @click="handleResBtnLocate">
-              <material-symbols-location-on-outline class="inline-block m-auto text-lg" />
-            </el-button>
-          </el-tooltip>
+    <div
+      v-loading="loading"
+      :element-loading-text="`校验中，请等待（${verifiedCount}/${verification.length}）`"
+    >
+      <el-table :data="gridData" table-layout="fixed" class="prochelper-table">
+        <el-table-column property="verifyObj" label="检查项" width="150" />
+        <el-table-column property="verifyInfo" label="检查内容" width="150" />
+        <el-table-column property="result" label="结果" />
+        <el-table-column property="detail" label="详细信息" />
+        <el-table-column label="操作" fixed="right" width="100">
+          <template #default>
+            <el-tooltip
+              class="box-item"
+              effect="light"
+              content="定位"
+              placement="top"
+            >
+              <el-button link type="primary" size="small" @click="handleResBtnLocate">
+                <material-symbols-location-on-outline class="inline-block m-auto text-lg" />
+              </el-button>
+            </el-tooltip>
 
-          <el-tooltip
-            class="box-item"
-            effect="light"
-            content="复制检查结果"
-            placement="top"
-          >
-            <el-button link type="primary" size="small">
-              <mingcute-copy-2-line class="inline-block m-auto text-lg" />
-            </el-button>
-          </el-tooltip>
-        </template>
-      </el-table-column>
-    </el-table>
+            <el-tooltip
+              class="box-item"
+              effect="light"
+              content="复制检查结果"
+              placement="top"
+            >
+              <el-button link type="primary" size="small">
+                <mingcute-copy-2-line class="inline-block m-auto text-lg" />
+              </el-button>
+            </el-tooltip>
+          </template>
+        </el-table-column>
+      </el-table>
 
-    <el-collapse v-model="activeCollapse">
-      <el-collapse-item :title="`已折叠${gridData.length}项无异常结果，点击查看`" name="1">
-        <el-table :data="gridData" table-layout="fixed">
-          <el-table-column property="name" label="检查项" width="150" />
-          <el-table-column property="detail" label="结果" />
-          <el-table-column property="detail" label="操作" fixed="right">
-            <template #default>
-              <el-tooltip
-                class="box-item"
-                effect="light"
-                content="定位"
-                placement="top"
-              >
-                <el-button link type="primary" size="small" @click="handleResBtnLocate">
-                  <material-symbols-location-on-outline class="inline-block m-auto text-lg" />
-                </el-button>
-              </el-tooltip>
+      <el-collapse v-model="activeCollapse" class="mt-12">
+        <el-collapse-item :title="`已折叠${gridData.length}项无异常结果，点击查看`" name="1">
+          <el-table :data="gridData" table-layout="fixed" class="prochelper-table">
+            <el-table-column property="verifyObj" label="检查项" width="150" />
+            <el-table-column property="verifyInfo" label="检查内容" width="150" />
+            <el-table-column property="result" label="结果" />
+            <el-table-column property="detail" label="详细信息" />
+            <el-table-column label="操作" fixed="right" width="100">
+              <template #default>
+                <el-tooltip
+                  class="box-item"
+                  effect="light"
+                  content="定位"
+                  placement="top"
+                >
+                  <el-button link type="primary" size="small" @click="handleResBtnLocate">
+                    <material-symbols-location-on-outline class="inline-block m-auto text-lg" />
+                  </el-button>
+                </el-tooltip>
 
-              <el-tooltip
-                class="box-item"
-                effect="light"
-                content="复制检查结果"
-                placement="top"
-              >
-                <el-button link type="primary" size="small">
-                  <mingcute-copy-2-line class="inline-block m-auto text-lg" />
-                </el-button>
-              </el-tooltip>
-            </template>
-          </el-table-column>
-        </el-table>
-      </el-collapse-item>
-    </el-collapse>
+                <el-tooltip
+                  class="box-item"
+                  effect="light"
+                  content="复制检查结果"
+                  placement="top"
+                >
+                  <el-button link type="primary" size="small">
+                    <mingcute-copy-2-line class="inline-block m-auto text-lg" />
+                  </el-button>
+                </el-tooltip>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-collapse-item>
+      </el-collapse>
+    </div>
   </ElDrawer>
 </template>
 
